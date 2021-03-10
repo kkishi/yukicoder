dea97749e135cc046d3c96db92414a681a0e0c7d
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-03-10 20:47:07 +0100

    add solutions

--- problems/no/1233/main.cc
+++ problems/no/1233/main.cc
@@ -3,4 +3,25 @@
 #include "yukicoder.h"
 
 void Main() {
+  ints(n);
+  V<int> a(n);
+  cin >> a;
+  const int N = *max_element(all(a));
+  int tot = accumulate(all(a), int(0));
+
+  V<int> b(N + 1);
+  each(e, a)++ b[e];
+
+  V<int> dx(N + 1);
+  int ans = 0;
+  int x = 0;
+  rep(i, 1, N + 1) {
+    ans += b[i] * tot;
+    for (int j = i; j <= N; j += i) {
+      dx[j] += i * b[i];
+    }
+    x += dx[i];
+    ans -= x * b[i];
+  }
+  wt(ans);
 }
