0dda9ada6dbb88377a25e53d13971db94390cc5d
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-03-11 19:05:00 +0100

    add solutions

--- problems/no/1419/main.cc
+++ problems/no/1419/main.cc
@@ -1,6 +1,46 @@
 #include <bits/stdc++.h>
 
+#include "modint.h"
 #include "yukicoder.h"
 
+using mint = ModInt<>;
+
+// Computes 2^k % n.
+int rem(int n, int k) {
+  if (k == 0) return 1;
+  if (even(k)) {
+    int r = rem(n, k / 2);
+    return r * r % n;
+  }
+  return rem(n, k - 1) * 2 % n;
+}
+
+V<mint> Solve(int n, int k) {
+  int r = (rem(n, k) - 1 + n) % n;        // (2^k - 1) % n
+  mint x = (mint(2).Pow(k) - r - 1) / n;  // (2^k - 1) / n
+  V<mint> ans(n);
+  for (int i = 1; i < n; i += 2) ans[i] = x;
+  for (int i = 1; i <= r; i += 2) ans[i] += 1;
+  return ans;
+}
+
+// The possible positions after k moves look like:
+//   -(2^k - 1), ..., -5, -3, -1, 1, 3, 5, ..., (2^k - 1)
+// So it is sufficient if we can compute (2^k - 1) / n modulo 10^9+7 (1).
+// It seems difficult to calculate (1) directly. It turns out that 2^k % n
+// (2) can be computed easily, and using it we can compute (1).
 void Main() {
+  ints(n, k);
+  V<mint> x;
+  if (!even(n)) {
+    V<mint> y = Solve(n * 2, k);
+    x.resize(n);
+    rep(i, n) x[i] = y[i] + y[n + i];
+  } else {
+    x = Solve(n, k);
+  }
+  V<mint> ans(n);
+  rep(i, n) ans[i] = x[i] + x[(n - i) % n];
+  mint t = 1 / mint(2).Pow(k);
+  each(e, ans) wt(e * t);
 }
--- problems/no/1420/main.cc
+++ problems/no/1420/main.cc
@@ -3,4 +3,42 @@
 #include "yukicoder.h"
 
 void Main() {
+  ints(n, m);
+  V<int> a(m), b(m), y(m);
+  rep(i, m) cin >> a[i] >> b[i] >> y[i];
+  V<int> ans(n);
+  rep(i, 30) {
+    VV<pair<int, int>> g(n);
+    rep(j, m) {
+      int A = a[j] - 1;
+      int B = b[j] - 1;
+      int C = hasbit(y[j], i);
+      g[A].eb(B, C);
+      g[B].eb(A, C);
+    }
+    V<int> v(n, -1);
+    bool ok = true;
+    rep(j, n) if (v[j] == -1) {
+      Fix([&](auto rec, int node, int parent, int value) {
+        if (v[node] != -1) {
+          if (v[node] != value) {
+            ok = false;
+          }
+          return;
+        }
+        v[node] = value;
+        for (auto [child, x] : g[node]) {
+          if (child == parent) continue;
+          int next_value = x == 0 ? value : (1 - value);
+          rec(child, node, next_value);
+        }
+      })(j, -1, 0);
+    }
+    if (!ok) {
+      wt(-1);
+      return;
+    }
+    rep(j, n) ans[j] |= v[j] << i;
+  }
+  each(e, ans) wt(e);
 }
