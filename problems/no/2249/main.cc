7b60e84cdaf2e96ea5f7033d62de0b964ef1ef58
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-03-19 19:31:02 +0100

    add solutions

--- problems/no/2249/main.cc
+++ problems/no/2249/main.cc
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 
+#include "totient.h"
 #include "yukicoder.h"
 
 void Main() {
+  const int N = 10000000;
+  V<int> totient = Totient(N);
+  V<int> ans(N + 1);
+  rep(i, 2, N + 1) ans[i] = ans[i - 1] + ((i - 1) * 2 - totient[i]);
+  ints(t);
+  rep(t) {
+    ints(n);
+    wt(ans[n]);
+  }
 }
